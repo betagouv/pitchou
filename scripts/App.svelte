078d5b5344b5d24d50cf81b121eac48f99de6e58
David Bruant <davidbruant@protonmail.com>
2024-02-14 14:15:00 +0100

    ptites améliorations de perf

--- scripts/App.svelte
+++ scripts/App.svelte
@@ -1,12 +1,25 @@
 <script>
 	import AutoComplete from "simple-svelte-autocomplete"
 	export let espèces;
+
+	$: espèceToLabel = makeEspèceToLabel(espèces)
+
+	function espèceLabel(esp){
+		return `${esp["NOM_VERN"]} - <i>${esp["LB_NOM"]}</i>`
+	}
+
+	function makeEspèceToLabel(espèces){
+		return new Map(espèces.map(e => [e, espèceLabel(e)]))
+	}
 </script>
 
 <h1>Recherche d'une espèce</h1>
-<AutoComplete items="{espèces}" labelFunction={e =>
-	`${e["NOM_VERN"]} - <i>${e["LB_NOM"]}</i>`
-	}>
+<AutoComplete 
+	items="{espèces}" 
+	labelFunction={e => espèceToLabel.get(e)}
+	maxItemsToShowInList=20
+	cleanUserText=false
+>
 	<div slot="item" let:item let:label>
 		{@html label}
 	</div>
