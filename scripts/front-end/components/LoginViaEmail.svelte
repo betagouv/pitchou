9c1578d3ff7ea9773f0bbe230b7a88a576cd7e9d
David Bruant <davidbruant@protonmail.com>
2024-06-13 16:09:35 +0200

    Mise en place du DSFR avec notamment le premier squelette et la page d'accueil

--- scripts/front-end/components/LoginViaEmail.svelte
+++ scripts/front-end/components/LoginViaEmail.svelte
@@ -1,4 +1,6 @@
 <script>
+    import Squelette from './Squelette.svelte'
+
     /** @type {Set<string>} */
     export let authorizedEmailDomains
 
@@ -9,21 +11,31 @@
 
 </script>
 
+<Squelette nav={false}>
+    <div class="fr-grid-row fr-pt-6w fr-grid-row--center">
+        <div class="fr-col-8">
+            <h1>Connexion par email</h1>
 
-<h1>Connexion par email</h1>
+            <p>Saisissez votre adresse email ici et vous recevrez un email avec un lien secret pour vous connecter à Pitchou.</p>
+            <p>⚠️ Seules les adresses emails venant d'un de ces domaine peuvent recevoir un lien de connexion : 
+                {#each [...authorizedEmailDomains] as authorizedEmailDomain, i}
+                    {#if i !== 0} ,&nbsp; {/if}
+                    <code class="hostname">{authorizedEmailDomain}</code>
+                {/each}
+            </p>
+        </div>
+    </div>
 
-<p>Saisissez votre adresse email ici et vous recevrez un email avec un lien secret pour vous connecter à Pitchou.</p>
-<p>⚠️ Seules les adresses emails venant d'un de ces domaine peuvent recevoir un lien de connexion : 
-    {#each [...authorizedEmailDomains] as authorizedEmailDomain, i}
-        {#if i !== 0} ,&nbsp; {/if}
-        <code class="hostname">{authorizedEmailDomain}</code>
-    {/each}
-</p>
-
-<form on:submit|preventDefault={() => envoiEmailConnexion(email)}>
-    <input autocomplete="email" type="email" bind:value={email}>
-    <button>Obtenir un lien de connexion par email</button>
-</form>
+    <div class="fr-grid-row fr-pb-6w fr-grid-row--center">
+        <div class="fr-col-6">
+            <form on:submit|preventDefault={() => envoiEmailConnexion(email)}>
+                <label class="fr-label" for="email">Adresse email</label>
+                <input class="fr-input" autocomplete="email" type="email" id="email" bind:value={email}>
+                <button class="fr-btn">Obtenir un lien de connexion par email</button>
+            </form>
+        </div>
+    </div>
+</Squelette>
 
 
 <style lang="scss">
