98998b4f8753729a12bfa92c77830c18943c359d
David Bruant <davidbruant@protonmail.com>
2024-06-19 11:54:16 +0200

    Création du fichier de groupes d'espèces

--- outils/groupes-espèces.js
+++ outils/groupes-espèces.js
@@ -1,29 +1,74 @@
 //@ts-check
 
-import {readFile} from 'node:fs/promises'
-
+import {readFile, writeFile} from 'node:fs/promises'
 import {sheetRawContentToObjects, getODSTableRawContent} from 'ods-xlsx'
+import { dsvFormat } from 'd3-dsv';
+
+import '../scripts/types.js'
 
 /**
  * Cet outil prend le fichier data/ListeGroupesEspeces.ods et en fait un fichier json plus léger
  * qui contient les groupes d'espèces pour usage dans
  */
 
-const groupeEspècesODSArrayBuffer = (await readFile('data/ListeGroupesEspeces.ods')).buffer
+const lignesGroupeEspècesP = readFile('data/ListeGroupesEspeces.ods')
+    .then(({buffer: groupeEspècesODSArrayBuffer}) => getODSTableRawContent(groupeEspècesODSArrayBuffer))
+    .then(tableRaw => {
+        const sheetRaw = tableRaw.get('ListeGroupesEspeces')
 
-const tableRaw = await getODSTableRawContent(groupeEspècesODSArrayBuffer)
-const sheetRaw = tableRaw.get('ListeGroupesEspeces')
+        // Trouver la première ligne avec plus de 5 éléments (qui est la "vraie" première ligne, avec les noms de colonne)
+        const firstRowIndex = sheetRaw.findIndex(row => row.length >= 5)
+        const actualSheetRaw = sheetRaw.slice(firstRowIndex)
 
-// Trouver la première ligne avec plus de 5 éléments (qui est la "vraie" première ligne, avec les noms de colonne)
-const firstRowIndex = sheetRaw.findIndex(row => row.length >= 5)
-const actualSheetRaw = sheetRaw.slice(firstRowIndex)
+        return sheetRawContentToObjects(actualSheetRaw)
+    })
 
-const groupeEspècesSheet = sheetRawContentToObjects(actualSheetRaw)
+const espèceParNomScientifiqueP = readFile('data/liste_especes.csv', 'utf8')
+    .then(str => {
+        /** @type {Espèce[]} */
+        const espèces = dsvFormat(';').parse(str)
 
-console.log('groupeEspècesSheet', groupeEspècesSheet)
+        return new Map(espèces.map(e => {
+            return [e['LB_NOM'], e]
+        }))
+    })
+    
+const outputPath = 'data/groupes_especes.json'
 
-throw `PPP
-    - trouver les espèces en se basant sur leur nom scientifique
-        - warning sur ceux qu'on trouve pas
-    - créer le json
-`
+Promise.all([lignesGroupeEspècesP, espèceParNomScientifiqueP])
+.then(([lignesGroupeEspèces, espèceParNomScientifique]) => {
+    const jsonableOutput = Object.create(null)
+
+    const espècesNonReconnues = []
+
+    for(const ligne of lignesGroupeEspèces){
+        const {'Nom scientifique': nomScientifique, 'Nom du groupe': nomGroupe} = ligne
+        const espèceDuGroupe = espèceParNomScientifique.get(nomScientifique)
+        
+        let jsonableEspèce;
+        
+        if(espèceDuGroupe){
+            const {LB_NOM, CD_NOM} = espèceDuGroupe
+            jsonableEspèce = {LB_NOM, CD_NOM}
+        }
+        else{
+            espècesNonReconnues.push(ligne)
+            jsonableEspèce = nomScientifique
+        }
+
+        const groupe = jsonableOutput[nomGroupe] || []
+        groupe.push(jsonableEspèce)
+        jsonableOutput[nomGroupe] = groupe
+    }
+
+    if(espècesNonReconnues.length >= 1){
+        const set = new Set(espècesNonReconnues.map(e => e['Nom scientifique']))
+        console.log([...set].join('\n'))
+        console.log(set.size, 'espèces non reconnues', espècesNonReconnues.length, lignesGroupeEspèces.length)
+    }
+
+    const jsonOutput = JSON.stringify(jsonableOutput);
+
+    return writeFile(outputPath, jsonOutput, 'utf8');
+})
+.then(() => console.log(`Fichier ${outputPath} créé avec succès`))
