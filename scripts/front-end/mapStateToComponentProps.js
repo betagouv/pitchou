8554731082420b686ee3e63db98f2a568fdd7cdd
Fanny Cheung <dev@ynote.hk>
2024-08-20 15:29:16 +0200

    Ajoute un type SqueletteProps

--- scripts/front-end/mapStateToComponentProps.js
+++ scripts/front-end/mapStateToComponentProps.js
@@ -1,9 +1,13 @@
 /** @import {PitchouState} from '../front-end/store.js' */
 
 /**
+ * @typedef {Object} SqueletteProps
+ * @property {string | undefined} email
+ */
+/**
  * 
  * @param {PitchouState} state
- * @returns 
+ * @returns {SqueletteProps}
 */
 export const mapStateToSqueletteProps = (state) => {
     return {
