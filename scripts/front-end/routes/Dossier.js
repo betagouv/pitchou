1e8121bb13742580abc3261d61a025a72547aa13
Fanny Cheung <dev@ynote.hk>
2024-08-22 15:59:23 +0200

    Correction des types sur la page Dossier

--- scripts/front-end/routes/Dossier.js
+++ scripts/front-end/routes/Dossier.js
@@ -1,5 +1,6 @@
 //@ts-check
 
+import page from 'page'
 import { replaceComponent } from '../routeComponentLifeCycle.js'
 import store from '../store.js'
 import { svelteTarget } from '../config.js'
@@ -7,30 +8,44 @@ import { mapStateToSqueletteProps } from '../mapStateToComponentProps.js';
 
 import Dossier from '../components/screens/Dossier.svelte';
 
+/** @import {PitchouState} from '../store.js' */
+/** @import {DossierId} from '../../types/database/public/Dossier.ts' */
+/** @import {DossierComplet}  from '../../types.js' */
+
 /**
  * @param {Object} ctx
  * @param {Object} ctx.params
  * @param {string} ctx.params.dossierId
  */
 export default ({params: {dossierId}}) => {
+    const id = Number(dossierId)
+    const { state } = store
+    const { dossiers } = state 
+
+    // TODO: expliquer que le dossier n'existe pas ?
+    if (!dossiers) return page("/")
+
+    const dossier = dossiers.get(/** @type {DossierId} */(id))
+        
+    // TODO: expliquer que le dossier n'existe pas ?
+    if (!dossier) return page('/')
+        
     /**
      * 
-     * @param {import('../store.js').PitchouState} _ 
+     * @param {DossierComplet} dossier
      * @returns 
      */
-    function mapStateToProps({dossiers}){
-        return {
-            ...mapStateToSqueletteProps(store.state),
-            //@ts-ignore
-            dossier: dossiers.get(Number(dossierId)),
-        }
+    function getMapStateToProps(dossier){
+        return (/** @type {PitchouState} */state) => ({
+            ...mapStateToSqueletteProps(state),
+            dossier,
+        })
     }   
     
-    const dossier = new Dossier({
+    const pageDossier = new Dossier({
         target: svelteTarget,
-        props: mapStateToProps(store.state)
+        props: getMapStateToProps(dossier)(state),
     });
 
-    replaceComponent(dossier, mapStateToProps)
-
+    replaceComponent(pageDossier, getMapStateToProps(dossier))
 }
