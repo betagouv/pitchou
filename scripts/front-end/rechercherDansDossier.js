af5908c813c2a8ae367ef2d1b4b24110a827e2bf
David Bruant <davidbruant@protonmail.com>
2024-12-05 14:53:06 +0100

    Améliore recherche textuelle (#121)
    * Rajouter un cache pour l'index de dossiers
    
    * Création de l'index plus résilient

--- scripts/front-end/rechercherDansDossier.js
+++ scripts/front-end/rechercherDansDossier.js
@@ -31,7 +31,7 @@ const créerDossierIndexable = dossier => {
     return {
         id: id.toString(),
         number_demarches_simplifiées: number_demarches_simplifiées?.toString(),
-        nom_dossier: retirerAccents(nom_dossier),
+        nom_dossier: retirerAccents(nom_dossier || ''),
         communes: communes?.map(({name}) => retirerAccents(name || "")).join(" ") || "",
         nom: retirerAccents(nom || ""),
         déposant_nom: retirerAccents(déposant_nom || ""),
@@ -47,32 +47,43 @@ const créerDossierIndexable = dossier => {
     }
 }
 
+/** @type {Map<DossierComplet[], lunr.Index>} */
+const indexCache = new Map()
+
 /**
  * 
  * @param {DossierComplet[]} dossiers
  * @returns {lunr.Index}
  */
-export const créerIndexDossiers = dossiers => {
-    return lunr(function() {
-        // @ts-expect-error TS ne comprends pas qu'on a ajouté lunrfr
-        this.use(lunr.fr)
+const créerIndexDossiers = dossiers => {
+    if(indexCache.has(dossiers))
+        // @ts-expect-error TS ne comprend pas que le .get retourne un lunr.Index après un .has positif
+        return indexCache.get(dossiers)
+    else{
+        const index = lunr(function() {
+            // @ts-expect-error TS ne comprends pas qu'on a ajouté lunrfr
+            this.use(lunr.fr)
+    
+            this.ref("id")
+            this.field("nom_dossier")
+            this.field("number_demarches_simplifiées")
+            this.field("communes")
+            this.field("nom")
+            this.field("déposant_nom")
+            this.field("déposant_prénoms")
+            this.field("demandeur_personne_physique_prénoms")
+            this.field("demandeur_personne_physique_nom")
+            this.field("demandeur_personne_morale_raison_sociale")
+            this.field("activité_principale")
+    
+            for (const dossier of dossiers) {
+                this.add(créerDossierIndexable(dossier))
+            }
+        })
 
-        this.ref("id")
-        this.field("nom_dossier")
-        this.field("number_demarches_simplifiées")
-        this.field("communes")
-        this.field("nom")
-        this.field("déposant_nom")
-        this.field("déposant_prénoms")
-        this.field("demandeur_personne_physique_prénoms")
-        this.field("demandeur_personne_physique_nom")
-        this.field("demandeur_personne_morale_raison_sociale")
-        this.field("activité_principale")
-
-        for (const dossier of dossiers) {
-            this.add(créerDossierIndexable(dossier))
-        }
-    })
+        indexCache.set(dossiers, index)
+        return index
+    }
 }
 
 /**
