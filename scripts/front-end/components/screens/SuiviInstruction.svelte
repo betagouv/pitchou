ad156b73570d928a1b86ca67a7a1ca75cdeadd1c
David Bruant <davidbruant@protonmail.com>
2025-01-02 10:17:32 +0100

    fix fix

--- scripts/front-end/components/screens/SuiviInstruction.svelte
+++ scripts/front-end/components/screens/SuiviInstruction.svelte
@@ -358,7 +358,7 @@
                             activité_principale, rattaché_au_régime_ae,
                             enjeu_politique, enjeu_écologique,
                             évènementsPhase, prochaine_action_attendue_par }}
-                                {@const /* @type {DossierPhase} */ phase = évènementsPhase[0].phase}
+                                {@const phase = /** @type {DossierPhase} */ (évènementsPhase[0].phase)}
                                 <tr>
                                     <td><a href={`/dossier/${id}`}>Voir le dossier</a></td>
                                     <td>{formatLocalisation({communes, départements, régions})}</td>
