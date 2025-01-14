943ef80d55d60d4dc63f0c851f9d59325ac524fb
David Bruant <davidbruant@protonmail.com>
2025-01-14 16:43:00 +0100

    Configuration de Kanel - Ajout de Buffer pour pg_catalog.bytea

--- .kanelrc.cjs
+++ .kanelrc.cjs
@@ -1,6 +1,15 @@
 /** @type {import('kanel').Config} */
 module.exports = {
     customTypeMap: {
-      "pg_catalog.bytea": 'ArrayBuffer'
+        'pg_catalog.bytea': 'Buffer'
     },
-  };
+
+    preRenderHooks: [
+        function hook(output, InstantiatedConfig){
+
+            console.log('kanel', output)
+            console.log('config', InstantiatedConfig)
+            return output
+        }
+    ]
+}
