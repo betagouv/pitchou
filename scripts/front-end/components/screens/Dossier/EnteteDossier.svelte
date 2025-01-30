a5a171c0f02f7afcb9319487dc68da56cb86d351
David Bruant <davidbruant@protonmail.com>
2025-01-30 17:31:35 +0100

    Premier jet en-tête dossier finie

--- scripts/front-end/components/screens/Dossier/EnteteDossier.svelte
+++ scripts/front-end/components/screens/Dossier/EnteteDossier.svelte
@@ -1,64 +1,108 @@
 <script>
     /** @import {DossierRésumé} from '../../../../types/API_Pitchou' */
 
-    import {formatLocalisation, formatDéposant, phases, prochaineActionAttenduePar} from '../../../affichageDossier.js'
-    import {afficherBool, afficherString} from '../../../affichageValeurs.js'
-    import TagEnjeu from '../../TagEnjeu.svelte'
+    import {formatLocalisation, formatDéposant} from '../../../affichageDossier.js'
+    import {afficherString} from '../../../affichageValeurs.js'
+    import TagPhase from '../../TagPhase.svelte'
 
     /** @type {DossierRésumé} */
     export let dossier
 
 </script>
 
-<header>
-    <h1>{dossier.nom}</h1>
-    <div>
-        <span class="fr-icon-map-pin-2-fill" aria-hidden="true"></span>
-        {formatLocalisation(dossier)}
-    </div>
-    <div>
-        <span class="fr-icon-user-fill" aria-hidden="true"></span>
-        {formatDéposant(dossier)}
-    </div>
-    <div>
-        <span class="fr-icon-briefcase-fill" aria-hidden="true"></span>
-        {dossier.activité_principale}
-    </div>
-    <div>
-        <span class="fr-icon-account-pin-circle-fill" aria-hidden="true"></span>
-        {afficherString(dossier.prochaine_action_attendue_par)}
-    </div>
-    <div>
-        {#if dossier.enjeu_politique || dossier.enjeu_écologique}
-            {#if dossier.enjeu_politique}
-                <span class="fr-icon-bank-fill" aria-hidden="true"></span>
-                Enjeu politique
-            {/if}
-            {#if dossier.enjeu_écologique}
-                <span class="fr-icon-earth-fill" aria-hidden="true"></span>
-                Enjeu écologique
-            {/if}
-        {/if}
-    </div>
+<header class="fr-mb-2w">
+    <section>
 
-    {#if dossier.rattaché_au_régime_ae}
+        <h1 class="fr-mb-1v">{dossier.nom}</h1>
         <div>
-            <span class="fr-icon-pantone-fill" aria-hidden="true"></span>
-            Autorisation environnementale
+            <span class="fr-icon-map-pin-2-fill" aria-hidden="true"></span>
+            {formatLocalisation(dossier)}
         </div>
-    {/if}
+        <div>
+            <span class="fr-icon-user-fill" aria-hidden="true"></span>
+            {formatDéposant(dossier)}
+        </div>
+        <div>
+            <span class="fr-icon-briefcase-fill" aria-hidden="true"></span>
+            {dossier.activité_principale}
+        </div>
+    </section>
 
-    <!--
-    <div>
-        <span class="fr-icon-scales-3-fill" aria-hidden="true"></span>
-        Contentiex
-        {dossier.rattaché_au_régime_ae}
-    </div>
-    -->
+    <section>
 
+        <div>
+            <strong>Phase&nbsp;:&nbsp;</strong><TagPhase phase={dossier.phase}></TagPhase>
+        </div>
+        
+        <div>
+            <strong>Prochaine action de&nbsp;:&nbsp;</strong>
+            {afficherString(dossier.prochaine_action_attendue_par)}
+        </div>
+
+        <div class="enjeux">
+            <div>
+                {#if dossier.enjeu_politique || dossier.enjeu_écologique}
+                    {#if dossier.enjeu_politique}
+                        <div>
+                            <span class="fr-icon-bank-fill" aria-hidden="true"></span>
+                            Enjeu politique
+                        </div>
+                    {/if}
+                    {#if dossier.enjeu_écologique}
+                        <div>
+                            <span class="fr-icon-earth-fill" aria-hidden="true"></span>
+                            Enjeu écologique
+                        </div>
+                    {/if}
+                {/if}
+            </div>
+            <div>
+                <button class="fr-btn fr-btn--secondary fr-btn--sm fr-btn--icon-left fr-icon-chat-3-line">
+                    Commentaire
+                </button>
+            </div>
+        </div>
+
+        {#if dossier.rattaché_au_régime_ae}
+            <div>
+                <span class="fr-icon-pantone-fill" aria-hidden="true"></span>
+                Autorisation environnementale
+            </div>
+        {/if}
+
+        <!--
+        <div>
+            <span class="fr-icon-scales-3-fill" aria-hidden="true"></span>
+            Contentiex
+            {dossier.rattaché_au_régime_ae}
+        </div>
+        -->
+    </section>
 
 </header>
 
 <style lang="scss">
+    header{
+        display: flex;
+        flex-direction: row;
 
+        & > :nth-child(1){
+            flex: 2
+        }
+        & > :nth-child(2){
+            flex: 1
+        }
+
+        section > div {
+            margin-bottom: 0.7rem;
+        }
+
+        .enjeux{
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: space-between;
+        }
+
+    }
 </style>
