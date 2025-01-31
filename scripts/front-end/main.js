99f381554ccf112b18b1550ce0a74d0695015cbe
David Bruant <davidbruant@protonmail.com>
2025-01-31 14:40:40 +0100

    Suppression routes description et rédaction arrrêté préfectoral

--- scripts/front-end/main.js
+++ scripts/front-end/main.js
@@ -4,9 +4,7 @@ import page from 'page'
 
 import Accueil from './routes/Accueil.js'
 import Dossier from './routes/Dossier.js';
-import RedactionArretePrefectoral from './routes/RedactionArretePrefectoral.js';
 import DossierMessagerie from './routes/DossierMessagerie.js'
-import DossierDescription from './routes/DossierDescription.js'
 import SaisieEspèces from './routes/SaisieEspèces.js';
 import ImportHistoriqueNouvelleAquitaine from './routes/import-historique/NouvelleAquitaine.js'
 import PreremplissageDerogation from './routes/PreremplissageDerogation.js';
@@ -18,9 +16,7 @@ import { init } from './actions/main.js';
 page('/', Accueil)
 
 page('/dossier/:dossierId', Dossier)
-page('/dossier/:dossierId/description', DossierDescription)
 page('/dossier/:dossierId/messagerie', DossierMessagerie)
-page('/dossier/:dossierId/redaction-arrete-prefectoral', RedactionArretePrefectoral)
 
 page('/saisie-especes', SaisieEspèces)
 page('/preremplissage-derogation', PreremplissageDerogation)
