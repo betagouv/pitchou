d8b7f757c3a58e610c00ff0949d09ceb908fd96d
David Bruant <davidbruant@protonmail.com>
2025-02-03 11:40:04 +0100

    Ajustements visuels modale

--- scripts/front-end/components/Dossier/EnteteDossier.svelte
+++ scripts/front-end/components/Dossier/EnteteDossier.svelte
@@ -84,13 +84,13 @@
 <dialog aria-labelledby="fr-modal-title-modal-1" id="fr-modal-1" class="fr-modal">
     <div class="fr-container fr-container--fluid fr-container-md">
         <div class="fr-grid-row fr-grid-row--center">
-            <div class="fr-col-12 fr-col-md-8 fr-col-lg-6">
+            <div class="fr-col-12 fr-col-md-8 fr-col-lg-8">
                 <div class="fr-modal__body">
                     <div class="fr-modal__header">
                         <button class="fr-btn--close fr-btn" title="Fermer la fenêtre modale" aria-controls="fr-modal-1">Fermer</button>
                     </div>
                     <div class="fr-modal__content">
-                        <h1 id="fr-modal-title-modal-1" class="fr-modal__title"><span class="fr-icon-arrow-right-line fr-icon--lg"></span>Commentaire enjeux</h1>
+                        <h1 id="fr-modal-title-modal-1" class="fr-modal__title">Commentaire enjeux</h1>
                         <div class="commentaire-enjeux">
                             {dossier.commentaire_enjeu}
                         </div>
