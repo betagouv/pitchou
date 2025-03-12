b08ec5d29e285dad27d12afe7415558c85217164
David Bruant <davidbruant@protonmail.com>
2025-03-12 17:04:40 +0100

    nettoyages

--- scripts/server/générerFichierDepuisTemplate.js
+++ scripts/server/générerFichierDepuisTemplate.js
@@ -1,5 +1,3 @@
-//import {join} from 'node:path'
-
 import carbone from 'carbone'
 
 /**
@@ -11,9 +9,6 @@ import carbone from 'carbone'
 export default function générerFichier(templateBuffer, données){
     return new Promise((resolve, reject) => {
         const templateId = `template-${Math.random().toString(36).slice(2)}`
-        // carbone conventions
-        //const templatePath = process.cwd()
-        //const templateFullPath = join(templatePath, templateId);
 
         carbone.addTemplate(templateId, templateBuffer, (err) => {
             if(err){
