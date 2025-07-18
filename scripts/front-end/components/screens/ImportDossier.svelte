3c4f58542b34ab4397501a3f0003a81478752f25
Clémence Fernandez <clemencefrz@gmail.com>
2025-07-18 12:33:26 +0200

    Ajouter les liens de préremplissages

--- scripts/front-end/components/screens/ImportDossier.svelte
+++ scripts/front-end/components/screens/ImportDossier.svelte
@@ -1,15 +1,24 @@
 <script>
+    import { text } from 'd3-fetch';
+ 
+
   //@ts-check
   
   import Squelette from '../Squelette.svelte'
   import { getODSTableRawContent,  sheetRawContentToObjects, isRowNotEmpty } from '@odfjs/odfjs'
   /** @import { ComponentProps } from 'svelte' */
+  /** @import {DossierDemarcheSimplifiee88444} from "../../../types/démarches-simplifiées/DémarcheSimplifiée88444" */
 
   /** @type {ComponentProps<Squelette>['email']} */
   export let email = undefined
 
   /** @type {any[] | undefined} */
-  let dossiersACréer = undefined
+  let lignesTableauImport = undefined
+
+  /** @type {Map<any,string>} */
+  let ligneToLienPréremplissage = new Map()
+
+
 
   /**
    * @param {Event} event
@@ -38,13 +47,36 @@
       const lignes = [...sheetRawContentToObjects(rawDataTableauSuivi.filter(isRowNotEmpty)).values()]
 
       console.log( {lignes })
-      dossiersACréer = lignes
+      lignesTableauImport = lignes
 
       } catch (error) {
         console.error(`Une erreur est survenue pendant la lecture du fichier : ${error}`)
       }
     }
 
+  }
+
+
+  /**
+  * @param {any} ligne
+  */
+  async function handleOnClickForLigne(ligne) {
+
+    /** @type {Partial<DossierDemarcheSimplifiee88444>} */
+    const dossier = { 'Nom du projet': ligne['OBJET'], 'Question cachée pour des raisons techniques': JSON.stringify({'commentaire': ligne['Description avancement dossier avec dates']}) }
+
+    try {
+      const lien = await text('/lien-preremplissage', {
+        method: 'POST',
+        headers: {'content-type':'application/json'},
+        body: JSON.stringify(dossier),
+      })
+
+      ligneToLienPréremplissage.set(ligne,lien)
+      ligneToLienPréremplissage = ligneToLienPréremplissage
+    } catch (error) {
+      throw new Error(`Une erreur est survenue lors de la récupération du lien de préremplissage : ${error}`)
+    }
   }
 
 </script>
@@ -66,25 +98,33 @@
     <li>Commentaires sur les enjeux et la procédure (Annotation privée) => Description avancement dossier avec dates</li>
   </ul>
 
-  {#if dossiersACréer}
+  {#if lignesTableauImport}
   <h2>Toutes les lignes du tableau</h2>
     <div class="fr-table" id="table-0-component">
       <div class="fr-table__wrapper">
         <div class="fr-table__container">
           <div class="fr-table__content">
-            <table id="table-0">
+            <table id="table-0" class="tableau-dossier-a-creer">
               <caption> Lignes du tableau </caption>
               <thead>
                 <tr>
                   <th> Nom du projet (OBJET) </th>
                   <th> Commentaires sur les enjeux et la procédure </th>
+                  <th> Action </th>
                 </tr>
               </thead>
               <tbody>
-                {#each dossiersACréer as dossier}
+                {#each lignesTableauImport as ligne}
                   <tr id="table-0-row-key-1" data-row-key="1">
-                    <td>{dossier['OBJET']}</td>
-                    <td>{dossier['Description avancement dossier avec dates']}</td>
+                    <td>{ligne['OBJET']}</td>
+                    <td>{ligne['Description avancement dossier avec dates']}</td>
+                    <td>
+                      {#if ligneToLienPréremplissage.get(ligne)}
+                        <a class='fr-btn' href={ligneToLienPréremplissage.get(ligne)} target='_blank'>Créer dossier</a>
+                      {:else}
+                        <button type="button" class="fr-btn fr-btn--secondary" on:click={() => handleOnClickForLigne(ligne)}>Préparer préremplissage</button>
+                      {/if}
+                    </td>
                   </tr>
                 {/each}
               </tbody>
@@ -95,3 +135,13 @@
     </div>
   {/if}
 </Squelette>
+
+<style lang='scss'>
+  .tableau-dossier-a-creer {
+    th, td:not(:last-of-type) {
+      max-width: 15rem;
+      max-height: 2rem;
+      overflow: auto;
+    }
+  }
+</style>
