b6aa0220952453fd64a9a25b3557e7123105dde2
Clémence Fernandez <clemencefrz@gmail.com>
2025-07-18 09:44:24 +0200

    Ajout d'un input pour récupérer le fichier ods

--- scripts/front-end/components/screens/ImportDossier.svelte
+++ scripts/front-end/components/screens/ImportDossier.svelte
@@ -1,13 +1,33 @@
 <script>
   //@ts-check
+  
   import Squelette from '../Squelette.svelte'
-  /** @import {ComponentProps} from 'svelte' */
+  /** @import { ComponentProps } from 'svelte' */
 
   /** @type {ComponentProps<Squelette>['email']} */
   export let email = undefined
+
+  /**
+   * @param {Event} event
+  */
+  function handleFileChange(event) {
+  const target = event.target;
+    if (target instanceof HTMLInputElement && target.files) {
+      console.log(target.files);
+    } else {
+      console.error('Le champ de fichier est introuvable ou ne contient aucun fichier.')
+    }
+  }
 </script>
 
 <Squelette {email} nav={true} >
   <h1>Import de dossier</h1>
-  <p>Page d'import de dossier (à compléter).</p>
+  <div class="fr-upload-group">
+    <label class="fr-label" for="file-upload">
+        Ajouter un fichier
+        <span class="fr-hint-text">Formats supportés : ods.</span>
+    </label>
+    <input class="fr-upload" aria-describedby="file-upload-messages" type="file" id="file-upload" name="file-upload" accept=".ods" on:change={handleFileChange}>
+    <div class="fr-messages-group" id="file-upload-messages" aria-live="polite"></div>
+</div>
 </Squelette>
