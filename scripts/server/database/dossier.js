6aad5018f13b5775c3533e4bf96550583d37ac52
Clémence Fernandez <clemencefrz@gmail.com>
2025-07-28 14:49:24 +0200

    Ajout d'un commentaire pour question

--- scripts/server/database/dossier.js
+++ scripts/server/database/dossier.js
@@ -411,7 +411,7 @@ export function listAllDossiersComplets(databaseConnection = directDatabaseConne
                 if(id_fichier_espèces_impactées){
                     dossier.url_fichier_espèces_impactées = `/especes-impactees/${id_fichier_espèces_impactées}`
                     // s'il y a un fichier, ignorer le champ contenant un lien
-                    delete dossier.espèces_protégées_concernées
+                    delete dossier.espèces_protégées_concernées // Est-ce qu'on garde cette ligne ?
                 }
 
             }
