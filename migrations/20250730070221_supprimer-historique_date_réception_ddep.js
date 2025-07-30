0daecc2217fb7ca2be3246cc474915dd279d8890
Clémence Fernandez <clemencefrz@gmail.com>
2025-07-30 09:03:33 +0200

    !!Migration!! Supprimer la colonne historique_date_réception_ddep

---
+++ migrations/20250730070221_supprimer-historique_date_réception_ddep.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+
+    await knex.schema.alterTable('dossier', table => {
+        table.dropColumn('historique_date_réception_ddep')
+    })
+
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+    await knex.schema.alterTable('dossier', table => {
+        table.date('historique_date_réception_ddep')
+    })
+};
