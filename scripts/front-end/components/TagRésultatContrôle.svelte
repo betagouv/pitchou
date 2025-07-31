9d90b71ff26debf5e04a31d935f11369455e056f
David Bruant <davidbruant@protonmail.com>
2025-07-31 14:42:59 +0200

    Simplification et couleur neutre

--- scripts/front-end/components/TagRésultatContrôle.svelte
+++ scripts/front-end/components/TagRésultatContrôle.svelte
@@ -15,12 +15,6 @@
     /** @type {'SM' | 'MD'} */
     export let taille = 'SM'
 
-    /** @type {MouseEventHandler<HTMLButtonElement> | undefined} */
-    export let onClick = undefined
-
-    /** @type {boolean | undefined} */
-    export let ariaPressed = undefined
-
     /** @type {string[]} */
     export let classes = []
 
@@ -43,32 +37,15 @@
         'fr-tag',
         tailleToClass.get(taille),
         // @ts-ignore
-        résultatToClass.get(résultatContrôle),
+        résultatToClass.get(résultatContrôle) || 'résultat--autre',
         ...classes
     ]
 
 
-    /**
-     * Le DSFR rajoute ses propres listeners pour gérer les aria-pressed, mais on n'en a pas besoin
-     * alors, on désactive la propagation des évènements pour éviter des problèmes d'affichage
-    */
-    /** @type {MouseEventHandler<HTMLButtonElement>} */
-    function onClickWithDSFROverride(e){
-        if(onClick){
-            e.stopImmediatePropagation()
-            onClick(e)
-        }
-    }
 
 </script>
 
-{#if typeof onClick === 'function'}
-    <button class={clsx(allClasses)} aria-pressed={ariaPressed} on:click={onClickWithDSFROverride} type="button">{résultatContrôle}</button>
-{:else}
-    <p class={clsx(allClasses)}>{résultatContrôle}</p>
-{/if}
-
-
+<p class={clsx(allClasses)}>{résultatContrôle}</p>
 
 
 <style lang="scss">
@@ -77,59 +54,27 @@
     $couleur-résultat-contrôle-autre: var(--background-flat-beige-gris-galet);
 
     p{
-        white-space: nowrap;
-    }
-
-    p, button.fr-tag[aria-pressed="true"]{
-
         // DSFR override
         &, &:hover{
             background-image: none;
         }
 
+        &.résultat--en-cours,
+        &.résultat--trop-tard,
+        &.résultat--autre{
+            background-color: $couleur-résultat-contrôle-autre;
+            color: var(--text-inverted-beige-gris-galet);
+        }
+        
         &.résultat--conforme{
             background-color: $couleur-résultat-contrôle-conforme;
             color: var(--text-inverted-success);
-
-            &::after{
-                color: $couleur-résultat-contrôle-conforme;
-            }
         }
+
         &.résultat--non-conforme,
         &.résultat--non-conforme-pas-information{
             background-color: $couleur-résultat-contrôle-non-conforme;
             color: var(--text-inverted-error);
-            
-            &::after{
-                color: $couleur-résultat-contrôle-non-conforme;
-            }
         }
-    }
-
-    button.fr-tag[aria-pressed="false"]{
-        /*&.phase--accompagnement-amont{
-            color: $couleur-phase-accompagnement-amont;
-            border: 1px solid $couleur-phase-accompagnement-amont;
-        }
-        &.phase--étude-recevabilité{
-            color: $couleur-phase-étude-recevabilité;
-            border: 1px solid $couleur-phase-étude-recevabilité;
-        }
-        &.phase--instruction{
-            color: $couleur-phase-instruction;
-            border: 1px solid $couleur-phase-instruction;
-        }
-        &.phase--contrôle{
-            color: $couleur-phase-contrôle;
-            border: 1px solid $couleur-phase-contrôle;
-        }
-        &.phase--classé-sans-suite{
-            color: $couleur-phase-classé-sans-suite;
-            border: 1px solid $couleur-phase-classé-sans-suite;
-        }
-        &.phase--obligations-terminées{
-            color: $couleur-phase-obligations-terminées;
-            border: 1px solid $couleur-phase-obligations-terminées;
-        }*/
     }
 </style>
