1aae4e4fbb3d45e949eb5ade2583b4ee435c5362
David Bruant <davidbruant@protonmail.com>
2025-08-16 11:58:38 +0200

    Update scripts/front-end/before-ses-lockdown.js
    Co-authored-by: Clémence  <107830431+clemencefrz@users.noreply.github.com>

--- scripts/front-end/before-ses-lockdown.js
+++ scripts/front-end/before-ses-lockdown.js
@@ -4,7 +4,7 @@
  * https://www.npmjs.com/package/ses#ecosystem-compatibility
  * notamment à cause de la "override mistake"
  * Ce module est l'opportunité de faire le nécessaire avant l'appel à lockdown pour 
- * préserver la compatilibilté
+ * préserver la compatibilité
  */
 
 // SvelteSet, SvelteMap se cognent sur l'override mistake lors de l'initialisation de la première instance
