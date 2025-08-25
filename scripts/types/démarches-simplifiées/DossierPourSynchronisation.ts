b3b308ebfa6d5b9fa90bd09959bcebbfea0d907a
Clémence Fernandez <clemencefrz@gmail.com>
2025-08-25 14:50:59 +0200

    Mise à jour ts doc de DossierPourSynchronisation

--- scripts/types/démarches-simplifiées/DossierPourSynchronisation.ts
+++ scripts/types/démarches-simplifiées/DossierPourSynchronisation.ts
@@ -19,21 +19,13 @@ type DossierAvecDonnéesPersonnesEntreprisesInitializers<T = DossierMutator | Do
  * Représente le format des données issues de Démarches Simplifiées (DS) 
  * avant leur insertion ou mise à jour dans la base de données.
  * 
- * Contexte :
- * - Ce type est une version enrichie de `DossierMutator` (pour modification) 
- *   ou `DossierInitializer` (pour création).
- * - Les champs relatifs aux personnes/entreprises (`déposant`, 
- *   `demandeur_personne_physique`, `demandeur_personne_morale`) sont remplacés 
- *   par `DonnéesPersonnesEntreprises` afin de traiter séparément la création 
- *   et la réutilisation de ces entités.
- * 
  * Problème technique actuel :
  * - Les données des personnes/entreprises sont récupérées depuis DS,
  *   créées en base, puis réinjectées dans les dossiers à stocker.
- * - Ce couplage complique l’utilisation directe de `DossierMutator` ou `DossierInitializer`.
+ * - Ce couplage complique l’utilisation directe de `DossierMutator` ou `DossierInitializer` pour le type DossierType.
  * 
  * TODO :
- * - Suivi due l'issue : @see {@link https://github.com/betagouv/pitchou/issues/312}
+ * - Suivi de l'issue : @see {@link https://github.com/betagouv/pitchou/issues/312}
  */
 
 export type DossierPourSynchronisation<DossierType> = {
