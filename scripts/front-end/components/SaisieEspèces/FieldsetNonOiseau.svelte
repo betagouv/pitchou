9a23584487e97080304e85efbc6afa7fd786faec
David Bruant <davidbruant@protonmail.com>
2025-09-05 14:44:43 +0200

    suppression styles inutilisés

--- scripts/front-end/components/SaisieEspèces/FieldsetNonOiseau.svelte
+++ scripts/front-end/components/SaisieEspèces/FieldsetNonOiseau.svelte
@@ -221,12 +221,6 @@
             max-width: 10rem;
         }
 
-        label{
-            select{
-                max-width: 30em;
-            }
-        }
-
         table{
             // surcharge DSFR pour que l'autocomplete s'affiche correctement
             overflow: initial;
