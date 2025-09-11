e03379ecb8bb4cee213b80aaf92a5b31c754ad6c
Clémence Fernandez <clemencefrz@gmail.com>
2025-09-11 09:17:22 +0200

    Correction, bien importer la colonne Type dans DossierRésumé

--- scripts/server/database/dossier.js
+++ scripts/server/database/dossier.js
@@ -338,6 +338,8 @@ const colonnesDossierComplet = [
 
     "nombre_nids_compensés_dossier_oiseau_simple",
     "nombre_nids_détruits_dossier_oiseau_simple",
+
+    "type",
 /*    
     "historique_date_envoi_dernière_contribution",
     "historique_date_saisine_csrpn",
