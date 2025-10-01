e692ccb1699732a89365f3ac2689b9aa6f36e6f7
Clémence <107830431+clemencefrz@users.noreply.github.com>
2025-10-01 09:56:29 +0200

    Ajouter le fichier html de la déclaration d'accessibilité et le lier à la mention Accessibilité : non conforme dans le pied de page (#366)
    * Ajouter le fichier html de la déclaration d'accessibilité et le lier à la mention Accessibilité : non conforme dans le pied de page
    
    * Mettre la page html de la déclaration d'accessibilité dans un Squelette pour que ce soit plus joli
    
    * Supprimer le doctype en trop dans le html de la déclaration d'accessibilité

---
+++ scripts/front-end/components/screens/Accessibilite.svelte
@@ -0,0 +1,22 @@
+<script>
+    //@ts-check
+    import Squelette from '../Squelette.svelte'
+    import Loader from '../Loader.svelte'
+
+    /** @type {{ title?: string, html?: string }} */
+    let { title = 'Accessibilité', html = '' } = $props();
+</script>
+
+<Squelette nav={false} {title}>
+    <div class="fr-container fr-my-6w">
+        {#if html === ''}
+            <Loader></Loader>
+        {:else}
+            <div class="fr-content" aria-labelledby="titre-declaration" >
+                {@html html}
+            </div>
+        {/if}
+    </div>
+</Squelette>
+
+
