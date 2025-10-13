e33a390cd569570b33cb4081a9468e72f56fa003
Hannaeko <git@hannaeko.eu>
2025-10-13 14:49:52 +0200

    install playwright and add first basic test

---
+++ playwright.config.js
@@ -0,0 +1,30 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './tests/e2e',
+  fullyParallel: false,
+  forbidOnly: !!process.env.CI,
+  retries: 1,
+  workers: 1,
+  reporter: 'list',
+  timeout: 10_000,
+
+  use: {
+    baseURL: 'http://127.0.0.1:2648/',
+    trace: 'retain-on-failure',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: { width: 1920, height: 1080 },
+      },
+    },
+  ],
+});
