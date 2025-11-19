4492ce0699a6c800d688e374f5deeb648f6b94a1
Clémence Fernandez <clemencefrz@gmail.com>
2025-11-19 16:45:39 +0100

    Améliorer accessibiltié Impact Espèce

--- scripts/front-end/components/SaisieEspèces/ImpactEspèce.svelte
+++ scripts/front-end/components/SaisieEspèces/ImpactEspèce.svelte
@@ -78,8 +78,11 @@
 </script>
 
 <fieldset class="fr-fieldset fr-input-group fr-fieldset__element">
-    <legend class="fr-sr-only">Impact #{indexImpact} de l’espèce #{indexEspèce}</legend>
-
+    {#if indexImpact && indexEspèce}
+        <legend class="fr-sr-only">Impact #{indexImpact} de l’espèce #{indexEspèce}</legend>
+    {:else}
+        <legend class="fr-sr-only">Impact sur l'espèce {espèceClassification}</legend>
+    {/if}
     <div class="fr-fieldset__element fr-input-group fr-grid-row fr-grid-row--gutters">
         <div class="fr-col-md-5 fr-col-12">
             <label class="fr-label" for="input-espece-{indexEspèce}-impact-{indexImpact}">
