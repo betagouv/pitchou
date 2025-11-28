6cf24c2f8fd1e981e7086800b10fe48553114d15
Clémence Fernandez <clemencefrz@gmail.com>
2025-11-28 18:09:03 +0100

    Ajout de tous les champs non fichier et du champ fichier saisine

--- scripts/front-end/components/Dossier/Avis/FormulaireAjouterAvisExpert.svelte
+++ scripts/front-end/components/Dossier/Avis/FormulaireAjouterAvisExpert.svelte
@@ -1,5 +1,4 @@
 <script>
-
     /** @import Dossier from "../../../../types/database/public/Dossier.ts" */
     /** @import { AvisExpertInitializer } from "../../../../types/database/public/AvisExpert" */
     import { ajouterAvisExpert as _ajouterAvisExpert } from "../../../actions/avisExpert"
@@ -14,8 +13,11 @@
     /** @type {Props} */
     let { onClickRetour, dossier } = $props();
 
-    /** @type {AvisExpertInitializer['expert']}*/
-    let expert = $state()
+    /** @type {Pick<AvisExpertInitializer, "expert" | "date_saisine" | "avis" | "date_avis">} */
+    let avisExpertÀAjouter = $state({})
+
+    /** @type {FileList | undefined} */
+    let filesFichierSaisine = $state()
 
     /** @type {string | null} */
     let messageErreur = $state(null)
@@ -27,12 +29,19 @@
      */
     async function ajouterAvisExpert(e) {
         e.preventDefault()
-
+        console.log("click sur ajouter AvisExpert")
         /** @type { AvisExpertInitializer } */
-        const nouvelAvisExpert = { dossier: dossier.id, expert: expert ?? '' }
-        
+        const nouvelAvisExpert = { dossier: dossier.id, ...avisExpertÀAjouter }
+
+        /** @type {File | undefined} */
+        let fichierSaisine
+
+        if (filesFichierSaisine && filesFichierSaisine.length >= 1) {
+            fichierSaisine = filesFichierSaisine[0]
+        }
+
         try {
-            await _ajouterAvisExpert(nouvelAvisExpert)
+            await _ajouterAvisExpert(nouvelAvisExpert, fichierSaisine)
             await refreshDossierComplet(dossier.id)
         } catch (e) {
             //@ts-ignore
@@ -48,12 +57,45 @@
         <div class="fr-fieldset__element">
         <div class="fr-input-group" id="champ-expert-group">
             <label class="fr-label" for="champ-expert">Expert</label>
-            <input bind:value={expert} class="fr-input" aria-describedby="champ-expert-messages" name="input" id="champ-expert" type="text">
+            <input bind:value={avisExpertÀAjouter.expert} class="fr-input" aria-describedby="champ-expert-messages" name="input" id="champ-expert" type="text">
             <div class="fr-messages-group" id="champ-expert-messages" aria-live="polite">
+            </div>
+        </div>
+        <div class="fr-upload-fichier-saisine-group">
+            <label class="fr-label" for="upload-fichier-saisine">Fichier de la saisine
+                <span class="fr-hint-text">Indication : 
+                    Taille maximale&nbsp;: 15 Mo. 
+                    Formats supportés&nbsp;: pdf</span>
+            </label>
+            <input accept=".pdf" bind:files="{filesFichierSaisine}" class="fr-upload" aria-describedby="upload-fichier-saisine-messages" type="file" id="upload-fichier-saisine" name="upload">
+            <div class="fr-messages-group" id="upload-fichier-saisine-messages" aria-live="polite">
+            </div>
+        </div>
+        <div class="fr-input-group" id="champ-date-saisine-group">
+            <label class="fr-label" for="input-champ-date-saisine"> Date saisine </label>
+            <input bind:value={avisExpertÀAjouter.date_saisine} class="fr-input" aria-describedby="input-champ-date-saisine-messages" id="input-champ-date-saisine" type="date">
+            <div class="fr-messages-group" id="input-champ-date-saisine-messages" aria-live="polite">
+            </div>
+        </div>
+        <div class="fr-input-group" id="champ-avis-group">
+            <label class="fr-label" for="champ-avis">Avis</label>
+            <input bind:value={avisExpertÀAjouter.avis} class="fr-input" aria-describedby="champ-avis-messages" name="input" id="champ-avis" type="text">
+            <div class="fr-messages-group" id="champ-avis-messages" aria-live="polite">
+            </div>
+        </div>
+        <div class="fr-input-group" id="champ-date-avis-group">
+            <label class="fr-label" for="input-champ-date-avis"> Date avis </label>
+            <input bind:value={avisExpertÀAjouter.date_avis} class="fr-input" aria-describedby="input-champ-date-avis-messages" id="input-champ-date-avis" type="date">
+            <div class="fr-messages-group" id="input-champ-date-avis-messages" aria-live="polite">
             </div>
         </div>
         </div>
         <div class="fr-messages-group" id="formulaire-ajouter-avis-expert-fieldset-messages" aria-live="polite">
+            {#if messageErreur}
+                <div class="fr-alert fr-alert--error fr-alert--sm">
+                    <p>{messageErreur}</p>
+                </div>
+            {/if}
         </div>
         <ul class="fr-btns-group fr-btns-group--inline">
             <li>
@@ -64,9 +106,4 @@
             </li>
         </ul>
     </fieldset>
-    {#if messageErreur}
-        <div class="fr-alert fr-alert--error fr-alert--sm">
-            <p>{messageErreur}</p>
-        </div>
-     {/if}
 </form>
