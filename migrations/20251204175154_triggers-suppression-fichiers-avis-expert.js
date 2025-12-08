7fc147066577bbbba5803bfb2ada7756b73cc628
Clémence Fernandez <clemencefrz@gmail.com>
2025-12-08 11:24:20 +0100

    Faire un trigger sur un événement Update de la table avis_expert pour supprimer les fichiers d'avis expert orphelins

---
+++ migrations/20251204175154_triggers-suppression-fichiers-avis-expert.js
@@ -0,0 +1,70 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+    await knex.raw(`
+        CREATE OR REPLACE FUNCTION supprimer_fichiers_avis_expert()
+        RETURNS TRIGGER
+        LANGUAGE PLPGSQL
+AS
+$$
+BEGIN
+	DELETE FROM fichier WHERE fichier.id = OLD.avis_fichier OR fichier.id = OLD.saisine_fichier;
+	return OLD;
+END;
+$$;
+
+DROP TRIGGER IF EXISTS supprimer_fichiers_avis_expert_trigger on public.avis_expert;
+
+CREATE TRIGGER supprimer_fichiers_avis_expert_trigger
+	AFTER DELETE
+	ON "avis_expert"
+	FOR EACH ROW
+EXECUTE PROCEDURE supprimer_fichiers_avis_expert();
+    `);
+
+    await knex.raw(`CREATE OR REPLACE FUNCTION supprimer_fichiers_avis_expert_orphelins()
+RETURNS TRIGGER
+LANGUAGE PLPGSQL
+AS
+$$
+BEGIN
+	IF OLD.saisine_fichier IS NOT NULL AND OLD.saisine_fichier <> NEW.saisine_fichier THEN
+		DELETE FROM fichier WHERE fichier.id = OLD.saisine_fichier;
+	END IF;
+	IF OLD.avis_fichier IS NOT NULL AND OLD.avis_fichier <> NEW.avis_fichier THEN
+		DELETE FROM fichier WHERE fichier.id = OLD.avis_fichier;
+	END IF;
+	return OLD;
+END;
+$$;
+
+DROP TRIGGER IF EXISTS supprimer_fichiers_avis_expert_orphelins_trigger on public.avis_expert;
+
+CREATE TRIGGER supprimer_fichiers_avis_expert_orphelins_trigger
+	AFTER UPDATE
+	ON "avis_expert"
+	FOR EACH ROW
+EXECUTE PROCEDURE supprimer_fichiers_avis_expert_orphelins();`);
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+    await knex.raw(`
+DROP TRIGGER IF EXISTS supprimer_fichiers_avis_expert_trigger 
+ON public.avis_expert;
+
+DROP FUNCTION IF EXISTS supprimer_fichiers_avis_expert();
+`)
+
+    await knex.raw(`
+DROP TRIGGER IF EXISTS supprimer_fichiers_avis_expert_orphelins_trigger 
+ON public.avis_expert;
+
+DROP FUNCTION IF EXISTS supprimer_fichiers_avis_expert_orphelins();
+`)
+} 
