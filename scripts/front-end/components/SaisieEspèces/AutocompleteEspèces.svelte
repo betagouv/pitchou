1101fd3983cfed2468c80df30bbc0aac5111c0aa
hannaeko <git@hannaeko.eu>
2025-12-15 12:40:27 +0100

    Améliorer l'autocomplete (#433)
    * wip: combobox avec autocomplete en liste
    
    * fix svlete warning
    
    * fix comportement au click
    
    * gérer les caractères affichables
    
    * améliorer la navigation clavier et fix bug reselection même espèce
    
    * commenter les logs
    
    * fix scroll pendant la navigation avec les fleches
    
    * supprime une variable redondante
    
    * implémente aria-activedescendant
    
    * ajout description
    
    * amélioration automcomplete
    
    * messages lives
    * afficher "pas de résultat"
    * suppression aria-activedescendant
    
    * changer aria-hidden par aria-disabled
    
    * Suppression de commentaire
    
    ---------
    
    Co-authored-by: Clémence Fernandez <clemencefrz@gmail.com>

--- scripts/front-end/components/SaisieEspèces/AutocompleteEspèces.svelte
+++ scripts/front-end/components/SaisieEspèces/AutocompleteEspèces.svelte
@@ -1,5 +1,5 @@
 <script>
-	/** @import {EspèceProtégée} from '../../../types/especes' */
+    /** @import {EspèceProtégée} from '../../../types/especes' */
 
     import { normalizeNomEspèce, normalizeTexteEspèce } from '../../../commun/manipulationStrings'
     import { espèceLabel } from '../../../commun/outils-espèces.js'
@@ -14,46 +14,150 @@
 
 
     /**
-     * il y a 1000 opportunités d'optimizations en temps
-     * (notamment des memoization). Une autre fois
-     */
+     * Ressources utilisées / inspirations:
+     * - https://a11y-guidelines.orange.com/fr/articles/recommandations-autocompletion/
+     * - https://alphagov.github.io/accessible-autocomplete/examples/
+     * - https://www.w3.org/WAI/ARIA/apg/patterns/combobox/examples/combobox-autocomplete-list/
+    */
 
-	/** @type {Props} */
+    /** @type {Props} */
 
     let {
         espèces,
         onChange,
-        id,
+        id = '',
         espèceSélectionnée = $bindable(undefined)
     } = $props()
 
-    $inspect('espèceSélectionnée', espèceSélectionnée)
+    let text = $state('')
+    let statusMessage = $state('')
 
-    let text = $derived(espèceSélectionnée ? espèceLabel(espèceSélectionnée) : '')
+    /** @type {number | null}*/
+    let selectedOption = $state(null)
 
-    $inspect('text', text)
+    let showListBox = $state(false)
 
-    let openChoices = $state(false);
-
-    /** @type {ReturnType<setTimeout> | undefined} */
-    let timeout;
-
-    let onfocus = () => {
-        clearTimeout(timeout)
-        openChoices = true
+    function onInputFocus() {
+        showListBox = text.length > 0 && espèceSélectionnée === undefined
+        selectedOption = null
     }
 
-    let onblur = () => {
-        timeout = setTimeout(
-            () => {openChoices = false},
-            400
-        )
+    function onInput() {
+        showListBox = true
+        espèceSélectionnée = undefined
+
+        if (text.length === 0) {
+            showListBox = false
+            espècesPertinentes = []
+        } else {
+            espècesPertinentes = filtrerEspeces(text)
+            if (espècesPertinentes.length === 0) {
+                messageLive('Pas de résultat')
+            } else {
+                messageLive(`${espècesPertinentes.length} résultats disponibles`)
+            }
+        }
     }
 
-    let espècesPertinentes = $derived.by(() => {
+    function onInputBlur() {
+        if (selectedOption === null) {
+            showListBox = false
+        }
+    }
+
+    /**
+     * @param {FocusEvent} e
+     * @param {number} indexOption
+     */
+    function onOptionBlur(e, indexOption) {
+        const focusInput = e.relatedTarget === input
+        const focusOtherOption = selectedOption !== indexOption && selectedOption !== null
+
+        if (!focusInput && !focusOtherOption) {
+            showListBox = false
+            selectedOption = null
+        }
+    }
+
+    /**
+     * @param {EspèceProtégée} espèce
+     */
+    function onOptionClick(espèce) {
+        selectionnerEspèce(espèce)
+    }
+
+    /**
+     * @param {MouseEvent} e
+     */
+    function onOptionMouseDown(e) {
+        // Évite la perte du focus et la fermeture de liste d'option
+        e.preventDefault()
+    }
+
+    /**
+     * @param {number | null} elementToFocus
+     */
+    function focusElement(elementToFocus) {
+        if (elementToFocus === null) {
+            input.focus()
+        } else {
+            optionsRefs[elementToFocus].focus()
+        }
+    }
+
+    /**
+     * @param {KeyboardEvent} e
+     */
+    function onKeyDown(e) {
+        switch (e.key) {
+            case "ArrowUp":
+                if (showListBox && selectedOption !== null) {
+                    e.preventDefault()
+                    selectedOption = selectedOption === 0 ? null : selectedOption - 1
+                    focusElement(selectedOption)
+                }
+                break
+
+            case "ArrowDown":
+                if (showListBox && espècesPertinentes.length > 0 && selectedOption !== espècesPertinentes.length - 1) {
+                    e.preventDefault()
+                    selectedOption = selectedOption === null ? 0 : selectedOption + 1
+                    focusElement(selectedOption)
+                }
+                break
+            case "Escape":
+                input.focus()
+                showListBox = false
+                break
+            case "Enter":
+                if (showListBox) {
+                    e.preventDefault()
+                    if (selectedOption !== null) {
+                        selectionnerEspèce(espècesPertinentes[selectedOption])
+                    }
+                }
+                break
+            case "ArrowLeft":
+            case "ArrowRight":
+            case "End":
+            case "Home":
+                input.focus()
+                break
+            default:
+                if (e.target !== input && e.key.length === 1) {
+                    input.focus()
+                }
+                break
+        }
+    }
+
+    /** @type {EspèceProtégée[]} */
+    let espècesPertinentes = $state([])
+
+    /** @param {string} text */
+    function filtrerEspeces(text) {
         if(text.trim().length === 0)
             return []
-
 
         return espèces
             .filter(({nomsScientifiques, nomsVernaculaires}) => {
@@ -76,7 +180,15 @@
                 })
             })
             .slice(0, 12)
-    })
+    }
+
+    /**
+     * @param {string} text
+     */
+    function messageLive(text) {
+        statusMessage = text
+        setTimeout(() => { statusMessage = ''}, 100)
+    }
 
     /**
      *
@@ -88,6 +200,10 @@
         }
 
         espèceSélectionnée = espèce
+        text = espèceLabel(espèceSélectionnée)
+        espècesPertinentes = []
+        input.focus()
+        showListBox = false
     }
 
     export function focus() {
@@ -99,55 +215,102 @@
      */
     let input;
 
+    /**
+     * @type {HTMLElement[]}
+     */
+    let optionsRefs = $state([]);
+
 </script>
 
 <div class="autocomplete-container" title={text}>
-    <input bind:this={input} bind:value={text} {onfocus} {onblur} id="{id ? id : ''}" class="fr-input">
+    <input
+        id="{ id }"
+        class="fr-input"
+        role="combobox"
+        autocomplete="off"
+        aria-expanded="{showListBox && espècesPertinentes.length > 0}"
+        aria-controls="combobox-{ id }-option-list"
+        aria-autocomplete="list"
+        aria-describedby="{text.length > 0 ? '' : `combobox-${ id }-help`}"
+        onfocus={onInputFocus}
+        onblur={onInputBlur}
+        onkeydown={onKeyDown}
+        oninput={onInput}
+        bind:this={input}
+        bind:value={text}
+    >
 
-    {#if openChoices && espècesPertinentes.length >= 1}
-    <ol>
-        {#each espècesPertinentes as espèce}
-            <li><button type="button" onclick={() => selectionnerEspèce(espèce)} {onfocus} {onblur}>
+    <ul
+        id="combobox-{ id }-option-list"
+        aria-labelledby="{ id }"
+        role="listbox"
+        hidden={!showListBox}
+    >
+        {#each espècesPertinentes as espèce, indexOption}
+            <li
+                role="option"
+                aria-selected="{ indexOption === selectedOption }"
+                aria-posinset="{ indexOption + 1 }"
+                aria-setsize="{ espècesPertinentes.length }"
+                tabindex="-1"
+                onblur={(e) => onOptionBlur(e, indexOption)}
+                onclick={() => onOptionClick(espèce)}
+                onkeydown={onKeyDown}
+                onmousedown={onOptionMouseDown}
+                bind:this={optionsRefs[indexOption]}
+            >
                 {espèceLabel(espèce)}
-            </button></li>
+            </li>
         {/each}
-    </ol>
-    {/if}
+
+        {#if espècesPertinentes.length === 0}
+            <li
+                role="option"
+                aria-disabled="true"
+                aria-selected="false"
+            >
+                Pas de résultat
+            </li>
+        {/if}
+    </ul>
+
+    <div aria-live="polite" aria-atomic="true" class="fr-sr-only">
+        {#if statusMessage }
+            {statusMessage}
+        {/if}
+    </div>
+
+    <span id="combobox-{ id }-help" hidden>
+        Utilisez les flèches « haut » et « bas » pour naviguer entres les suggestions et « entrer » pour sélectionner.
+    </span>
+
 </div>
 
 <style lang="scss">
     .autocomplete-container{
         position: relative;
 
-        ol{
+        ul {
             position: absolute;
-            width: 300%;
+            width: 100%;
+            margin: 0;
 
             z-index: 1;
-
             background-color: var(--border-default-grey);
-
             padding-inline-start: 0;
 
             li{
                 width: 100%;
+                cursor: pointer;
 
                 background-color: var(--background-contrast-grey);
+                list-style-type: none;
+                padding: 0.3rem;
 
-                &::marker{
-                    content: none;
-                }
-
-                button{
-                    width: 100%;
-                    height: 100%;
-                    padding: 0.3rem;
-
-                    text-align: left;
+                &[aria-selected="true"], &:hover {
+                    background-color: var(--background-contrast-grey-active);
                 }
             }
         }
     }
-
-
 </style>
