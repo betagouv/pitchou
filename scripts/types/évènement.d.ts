4f30aa3b9c49b8b3b24ea09798fe8edc8f0e139e
Hannaeko <git@hannaeko.eu>
2026-01-06 18:10:33 +0100

    ajout de commentaire pour décrire les évènements

--- scripts/types/évènement.d.ts
+++ scripts/types/évènement.d.ts
@@ -1,7 +1,9 @@
 export type ÉvènementMétrique = {
+    // Cliquer sur un lien de connexion
     type: 'seConnecter',
     détails: undefined
 } | {
+    // Appuyer sur un bouton pour suivre un dossier
     type: 'suivreUnDossier',
     détails: {
         dossierId: number
