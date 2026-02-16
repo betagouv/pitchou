fec225bc786b90013ba86c3a7d7693ad3ce02471
Clémence Fernandez <clemencefrz@gmail.com>
2026-02-16 10:39:32 +0100

    Test E2E - ajout de plusieurs dossiers

--- tests/e2e/page-mes-dossiers/données.sql
+++ tests/e2e/page-mes-dossiers/données.sql
@@ -2,17 +2,35 @@ DO $$
 DECLARE
     code_acces text := 'abyssin';
     numero_demarche int := 88444;
-    dossier_id int;
+    dossier_ids int[];
     groupe_id uuid;
     cap_dossier uuid;
+    personne_id int;
 BEGIN
+
+    -- Instructeurice
     INSERT INTO personne (email, "code_accès")
-    VALUES ('alexandre@example.net', code_acces);
+    VALUES ('alexandre@example.net', code_acces)
+    RETURNING id INTO personne_id;
 
-    INSERT INTO dossier (nom, "numéro_démarche", "date_dépôt")
-    VALUES ('Parc photovoltaïque à Anglet', numero_demarche, now())
-    RETURNING id INTO dossier_id;
+    -- Dossiers
+    WITH dossiers_inseres AS (
+        INSERT INTO dossier (nom, "numéro_démarche", "date_dépôt")
+        SELECT nom, numero_demarche, now()
+        FROM (VALUES
+            ('Parc photovoltaïque à Anglet'),
+            ('Recherche scientifique sur les chats'),
+            ('Projet hydraulique'),
+            ('Projet de carrières à Abyssin')
+        ) AS v(nom)
+        RETURNING id
+    )
 
+
+    SELECT array_agg(id) INTO dossier_ids
+    FROM dossiers_inseres;
+
+    -- Groupe Instructeurice
     INSERT INTO groupe_instructeurs (nom, "numéro_démarche")
     VALUES ('DREAL Pays Basque', numero_demarche)
     RETURNING id INTO groupe_id;
@@ -21,9 +39,13 @@ BEGIN
     VALUES (code_acces)
     RETURNING cap INTO cap_dossier;
 
-    INSERT INTO "arête_groupe_instructeurs__dossier"
-    VALUES (dossier_id, groupe_id);
+    INSERT INTO "arête_groupe_instructeurs__dossier" (dossier, groupe_instructeurs)
+    SELECT unnest(dossier_ids), groupe_id;
 
     INSERT INTO "arête_cap_dossier__groupe_instructeurs"
     VALUES (cap_dossier, groupe_id);
+
+    -- Personne suit dossier
+    INSERT INTO "arête_personne_suit_dossier" (personne, dossier)
+    SELECT personne_id, unnest(dossier_ids);
 END $$;
--- tests/e2e/page-mes-dossiers/test.spec.js
+++ tests/e2e/page-mes-dossiers/test.spec.js
@@ -12,16 +12,18 @@ test.describe('Page Mes dossiers', () => {
 
         await expect(page.getByRole('heading', { level: 1})).toContainText('Tableau de suivi instruction DDEP');
 
-        await expect(page.getByRole('heading', { name: '/1 dossiers sélectionnés', level: 2 })).toContainText('1/1 dossiers sélectionnés')
+        await expect(page.getByRole('heading', { name: '/4 dossiers sélectionnés', level: 2 })).toContainText('4/4 dossiers sélectionnés')
         
         await page.getByRole('link', { name: 'Mes dossiers Nouveau' }).click()
 
         await expect(page.getByRole('heading', { level: 1})).toContainText('Mes dossiers');
     });
 
-    // TODO: Je vois tous les dossiers que je suis
+    // TODO: Je peux voir tous les dossiers que je suis et les dossiers selon le tri suivant :
+    // - D'abord par leur date de notification si la notification n'a pas été consultée, de la plus récente à la plus ancienne
+    // - Puis par date date_dépôt, de la plus récente à la plus ancienne
 
-    // TODO: Quand j'appuie sur Nouveauté, je ne vois que les dossiers avec des nouveautés
+    // TODO: Quand j'appuie sur Nouveauté, je ne vois que les dossiers qui possèdent une notification non vue.
 
     // TODO: Quand je consulte un dossier et que je reviens sur la page Mes dossiers, le tag nouveauté disparaît. 
 
