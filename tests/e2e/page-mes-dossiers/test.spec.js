1406d4e2b7dc3ae9c05166d99ddee2bac5107d2a
Clémence Fernandez <clemencefrz@gmail.com>
2026-02-16 09:40:39 +0100

    Test E2E - avois accès à la page mes dossiers

--- tests/e2e/page-mes-dossiers/test.spec.js
+++ tests/e2e/page-mes-dossiers/test.spec.js
@@ -2,20 +2,21 @@ import { test, expect } from '@playwright/test';
 import beforeAll from '../before-all';
 
 test.beforeAll(async () => {
-    await beforeAll('connexion-réussie/données.sql')
+    await beforeAll('page-mes-dossiers/données.sql')
 });
 
 
-test.describe('Connexion réussie', () => {
-    test(`Le code d'accès est valide`, async ({ page }) => {
+test.describe('Page Mes dossiers', () => {
+    test(`J'ai accès à la page "Mes dossiers"`, async ({ page }) => {
         await page.goto('/?secret=abyssin');
 
         await expect(page.getByRole('heading', { level: 1})).toContainText('Tableau de suivi instruction DDEP');
 
         await expect(page.getByRole('heading', { name: '/1 dossiers sélectionnés', level: 2 })).toContainText('1/1 dossiers sélectionnés')
-        await expect(page.locator('div.fr-table > table')).toBeVisible()
-        await expect(page.locator('div.fr-table > table > thead')).toBeVisible()
-        await expect(page.locator('div.fr-table > table > tbody > tr:first-child')).toBeVisible()
+        
+        await page.getByRole('link', { name: 'Mes dossiers Nouveau' }).click()
+
+        await expect(page.getByRole('heading', { level: 1})).toContainText('Mes dossiers');
     });
 
 });
