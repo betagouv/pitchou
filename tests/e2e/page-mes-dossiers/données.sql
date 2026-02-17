8f70327160cdda767088b014443b2d3d95ada736
Clémence Fernandez <clemencefrz@gmail.com>
2026-02-17 15:50:36 +0100

    Test - Les dossiers avec une notification non lue ont un tag Nouveauté

--- tests/e2e/page-mes-dossiers/données.sql
+++ tests/e2e/page-mes-dossiers/données.sql
@@ -20,8 +20,8 @@ BEGIN
         FROM (VALUES
             ('Parc photovoltaïque à Anglet', date '2026-02-14'),
             ('Recherche scientifique sur les chats', date '2026-01-14'),
-            ('Projet hydraulique', date '2026-02-18'),
-            ('Projet de carrières à Abyssin', date '2025-02-18'),
+            ('Projet hydraulique', date '2025-02-18'),
+            ('Projet de carrières à Abyssin', date '2026-02-18'),
             ('Dossier que je ne suis pas', now())
         ) AS v(nom, "date_dépôt")
         RETURNING id
@@ -52,7 +52,9 @@ BEGIN
 
     -- Notification : Seuls les 2 premiers dossiers ont une notification non vue.
     INSERT INTO "notification" (personne, dossier, vue, updated_at)
-    SELECT personne_id, unnest(dossier_ids[1:2]), 'false', now();
+    VALUES (personne_id, dossier_ids[1], 'false', date '2026-02-15'),
+    (personne_id, dossier_ids[2], 'false', date '2026-02-17');
+
     -- l'insert ci-dessous est nécessaire pour que l'instructeurice ait la bonne cap pour accéder aux notifications
     INSERT INTO "cap_évènement_métrique" (personne_cap)
     VALUES (code_acces);
