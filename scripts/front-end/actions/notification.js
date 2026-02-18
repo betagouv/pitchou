ccfd115a8c326458633fbf354ab9efec1c37ebae
Clémence Fernandez <clemencefrz@gmail.com>
2026-02-18 10:50:33 +0100

    - [x] Faire en sorte de ne pas lancer de requête si la notification est déjà à vue true

--- scripts/front-end/actions/notification.js
+++ scripts/front-end/actions/notification.js
@@ -6,8 +6,12 @@ import store from "../store";
  * @param { Pick<Notification, "dossier"> & Partial<Omit<Notification, 'dossier'>> } notification
  */
 export function updateNotificationForDossier(notification) {
-    if (store.state.capabilities.updateNotificationForDossier) {
-        store.state.capabilities.updateNotificationForDossier(notification)
-            .catch(e => console.warn(`Échec lors de la mise à jour de la notification :`, e, notification))
+    const updateNotificationForDossier = store.state.capabilities.updateNotificationForDossier
+
+    if (!updateNotificationForDossier) {
+        throw new Error(`Pas les droits suffisants pour mettre à jour la notification pour le dossier ${notification.dossier}. La notification : ${notification}.`)
     }
+
+    updateNotificationForDossier(notification)
+            .catch(e => console.warn(`Échec lors de la mise à jour de la notification :`, e, notification))
 }
