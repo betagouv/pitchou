b54a3cef4c56462cfec6a7672a42a53b9a4679e9
Clémence Fernandez <clemencefrz@gmail.com>
2026-02-18 14:16:07 +0100

    WIP - mettre les données des évènements dans un fichier ods

--- outils/aarri.js
+++ outils/aarri.js
@@ -3,6 +3,8 @@ import {writeFile} from 'node:fs/promises'
 import parseArgs from 'minimist'
 import { getPremièreSemaineActivéFromÉvènements, getÉvènementsCountForPersonne, getÉvènementsForPersonne } from '../scripts/server/database/aarri/utils.js';
 import {createOdsFile} from '@odfjs/odfjs'
+import { formatDateAbsolue } from '../scripts/front-end/affichageDossier.js';
+import { extraireNomDunMail } from '../scripts/front-end/actions/importDossierUtils.js';
 
 const DATABASE_URL = process.env.DATABASE_URL
 if(!DATABASE_URL){
@@ -18,54 +20,87 @@ if (!args.email) {
 
 const email = args.email
 
+console.log(`Mail de la personne concernée : ${email}`)
+console.log(`Début des Calculs des données AARRI.`)
+
 const évènements = await getÉvènementsForPersonne(email)
 const évènementsCount = await getÉvènementsCountForPersonne(email)
 const premièreSemaineActive = getPremièreSemaineActivéFromÉvènements(évènements)
 
-console.log('évènements: ', évènements.length, ', évènementsCount: ', évènementsCount.length)
-console.log('première semaine active : ', premièreSemaineActive)
+console.log(`✅ Résultats :`)
+console.log('Cette personne a enregistré', évènements.length, ' ','évènements depuis le',`${formatDateAbsolue(évènements.at(-1)?.date)}`)
 
-const évènementsColonneDate = évènements.map((évènement) => ([{
-    value: évènement.date,
-    type: 'string'
-}]));
+console.log(`📊 Données sur les phases AARRI :`)
+console.log('Première semaine active : ', formatDateAbsolue(premièreSemaineActive))
+
+
+// Création du fichier ODS pour stocker les résultats
+const évènementsFormattésPourODS = évènements.map( ({ date, évènement, détails } ) => ([
+    {
+      value: formatDateAbsolue(date, 'dd/MM/yyyy'),
+      type: 'string'
+    }, 
+    {
+      value: évènement,
+      type: 'string'
+    },
+    {
+      value: détails ? JSON.stringify(détails) : '',
+      type: 'string'
+    }
+]));
+
+const headerÉvènements = [[{
+  value: 'date',
+  type: 'string'
+}],
+[{
+  value: 'évènement',
+  type: 'string'
+}],
+[{
+  value: 'détails',
+  type: 'string'
+}]]
+const évènementCountsFormattésPourODS = évènementsCount.map( ({ évènement, count } ) => ([
+    {
+      value: évènement,
+      type: 'string'
+    },
+    {
+      value: count,
+      type: 'number'
+    },
+]));
 
 const content = new Map([
     [
-        'Évènements',
-        évènementsColonneDate,
+        'évènements',
+        [...headerÉvènements, ...évènementsFormattésPourODS]
     ],
     [
-        "L'autre feuille",
-        [
-            [
-                {value: évènements[0].date, type: 'string'},
-                {value: '2', type: 'string'},
-                {value: '3', type: 'string'},
-                {value: '5', type: 'string'},
-                {value: '8', type: 'string'}
-            ],
-            [
-                {value: '1', type: 'string'},
-                {value: '2', type: 'string'},
-                {value: '3', type: 'string'},
-                {value: '5', type: 'string'},
-                {value: '8', type: 'string'}
-            ]
-        ],
+        "évènements_count",
+        évènementCountsFormattésPourODS
     ]
 ])
 
 /** @type {ArrayBuffer} */
 const ods = await createOdsFile(content)
 
-async function example() {
+const { prénom, nom } = extraireNomDunMail(email)
+const aujourdhui = new Date()
+
+const nomDuFichier = `donnees-aarri${(prénom!='' || nom!=='') ? `-${prénom}-${nom}` : ''}-${formatDateAbsolue(aujourdhui,'dd-MM-yyyy')}.ods`
+
+async function créerFichierODS() {
   try {
-    await writeFile('./test.ods', Buffer.from(ods));
+    console.log('📝 Création du fichier ODS avec les résultats...')
+    await writeFile(`./${nomDuFichier}`, Buffer.from(ods));
+    console.log(`✅ Le fichier ${nomDuFichier} a bien été créé !`)
   } catch (err) {
     console.log(err);
   }
 }
-await example();
+await créerFichierODS();
 
 process.exit(0)
