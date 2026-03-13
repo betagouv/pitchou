5271ef2012fb35248f158141b448c23deecd50a5
Clémence Fernandez <clemencefrz@gmail.com>
2026-03-13 11:14:59 +0100

    améliorer description impact

--- scripts/server/database/aarri/personnes-par-phase.js
+++ scripts/server/database/aarri/personnes-par-phase.js
@@ -39,15 +39,12 @@ export async function getPersonnesActivesAvecSemaine() {
 
 /**
  * Retourne les personnes dans la phase Impact et la date à laquelle elles sont rentrées dans la phase Impact.
- * L'impact de Pitchou est mesuré par les retours à conformité
+ * Avoir de l'impact, c'est de faire au moins un contrôle qui produit un retour à la conformité
+ * donc un contrôle Conforme qui arrive après un contrôle qui est autre chose que Conforme
  * 
  * @returns {Promise<{id: Personne['id'], email: Personne['email'], semaine: Date}[]>} La liste des personnes dans la phase Impact et la date à laquelle elles sont rentrées dans la phase Impact.
 */
 export async function getPersonnesImpactAvecSemaine() {
-    /*
-        Avoir de l'impact, c'est de faire au moins un contrôle qui produit un retour à la conformité
-        donc un contrôle Conforme qui arrive après un contrôle qui est autre chose que Conforme
-    */
     /** @type {ÉvènementMétrique['type'][]} */
     const évènements = [ 'retourÀLaConformité' ]
     const nombreSeuil = 1
