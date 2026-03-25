4f0fe125e9ea068234010551ccb85f5f70ffe366
Clémence Fernandez <clemencefrz@gmail.com>
2026-03-25 11:51:30 +0100

    On crée les déclarations (ou on les met à jour) dans l'outil de synchronisation

--- scripts/server/database/espèces_impactées.js
+++ scripts/server/database/espèces_impactées.js
@@ -1,7 +1,9 @@
-import { resolve } from 'path';
+
 import {directDatabaseConnection} from '../database.js'
 
 /** @import {default as Fichier} from '../../../scripts/types/database/public/Fichier.ts' */
+/** @import {default as DéclarationEspècesImpactées, DClarationEspCesImpactEsInitializer} from '../../../scripts/types/database/public/DéclarationEspècesImpactées.ts' */
+/** @import {default as Dossier} from '../../../scripts/types/database/public/Dossier.ts' */
 /** @import {DossierDS88444} from '../../types/démarche-numérique/apiSchema.ts' */
 /** @import {Knex} from 'knex' */
 
@@ -11,10 +13,11 @@ const inutile = true;
 /**
  * 
  * @param {Map<DossierDS88444['number'], Fichier['id']>} espècesImpactéesParNuméroDossier
+ * @param {Map<DossierDS88444['number'], Dossier['id']>} dossierIdByDS_number
  * @param {Knex.Transaction | Knex} [databaseConnection]
  * @returns {Promise<any>}
  */
-export async function synchroniserFichiersEspècesImpactéesDepuisDS88444(espècesImpactéesParNuméroDossier, databaseConnection = directDatabaseConnection){
+export async function synchroniserFichiersEspècesImpactéesDepuisDS88444(espècesImpactéesParNuméroDossier, dossierIdByDS_number, databaseConnection = directDatabaseConnection){
     /** @type {Promise<any>} */
     let miseÀjourFichiersDansDossierP
 
@@ -47,19 +50,43 @@ export async function synchroniserFichiersEspècesImpactéesDepuisDS88444(espèc
         })
     }
 
-    // Trouver les fichiers déjà en place (pour les supprimer plus bas)
-    const fichiersIdPrécédents = await databaseConnection('déclaration_espèces_impactées')
-        .select(['fichier'])
-        .join('dossier', {'dossier.id': 'déclaration_espèces_impactées.dossier'})
-        .whereIn('dossier.number_demarches_simplifiées', [...espècesImpactéesParNuméroDossier.keys()]);
+    const espècesImpactéesIdDossier = [...espècesImpactéesParNuméroDossier.keys()].map((numéroDossierDN) => {
+        const idDossier = dossierIdByDS_number.get(numéroDossierDN)
+        if (idDossier) {
+            return idDossier
+        } else {
+            throw new Error(`Aucun id de dossier ne correspond au numéro DN : ${numéroDossierDN}`)
+        }
+    })
 
-    // // Associer les nouveaux fichiers espèces impactées au bon dossier
-    // const updatePs = [...espècesImpactéesParNuméroDossier].map(([numberDossier, fichierId]) => {
-    //     return databaseConnection('déclaration_espèces_impactées')
-    //         .join('dossier', {'dossier.id': 'déclaration_espèces_impactées.dossier'})
-    //         .insert({fichier: fichierId})
-    //         .where({'dossier.number_demarches_simplifiées': numberDossier});
-    // })
-    return miseÀjourFichiersDansDossierP
+    // Trouver les déclarations espèces impactées déjà en place (pour les supprimer plus bas)
+    /** @type {DéclarationEspècesImpactées['id'][]} */
+    const déclarationsEspècesImpactéesPrécédentes = (
+        await databaseConnection('déclaration_espèces_impactées')
+            .select(['id'])
+            .whereIn('dossier', espècesImpactéesIdDossier)
+        ).map(({id}) => id)
+
+    // Supprimer les anciennes déclarations espèces impactées.
+    // Le fichier des espaces impactées de la table fichier et lignes des espèces impactées dans la table espèce_impactée
+    // qui lui sont liées seront supprimés en cascade grâce au trigger mis en place.
+    await databaseConnection('déclaration_espèces_impactées')
+        .delete()
+        .whereIn('id', déclarationsEspècesImpactéesPrécédentes)
+
+    // Ajouter les nouvelles déclarations espèces impactées
+    /** @type {DClarationEspCesImpactEsInitializer[]} */
+    const déclarationsEspècesImpactéesÀInsérer = [...espècesImpactéesParNuméroDossier].map(([numéroDossierDN, fichierId]) => {
+            const dossierId = dossierIdByDS_number.get(numéroDossierDN)
+            if (dossierId) {
+                return {dossier: dossierId, fichier: fichierId}
+            } else {
+                throw new Error(`Aucun id de dossier ne correspond au numéro DN : ${numéroDossierDN}`)
+            }
+    })
+    const miseÀjourFichiersDansDéclarationsEspècesImpactéesP = databaseConnection('déclaration_espèces_impactées')
+        .insert(déclarationsEspècesImpactéesÀInsérer)
+
+    return Promise.all([miseÀjourFichiersDansDossierP, miseÀjourFichiersDansDéclarationsEspècesImpactéesP])
     
 }
