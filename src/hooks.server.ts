06edcca433a5f9a658d3a021a244e13dcd7ea296
Nicolas Cura <nicolas.cura@icloud.com>
2026-05-23 09:29:14 +0200

    feat(kit): hook serveur de boot

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,16 @@
 import type { Handle } from "@sveltejs/kit";
+import { miseEnPlaceSecretGeoMCE } from "$server/database/capability-geomce.js";
+
+if (!process.env.DEMARCHE_SIMPLIFIEE_API_TOKEN) {
+  throw new TypeError(`Variable d'environnement DEMARCHE_SIMPLIFIEE_API_TOKEN manquante`);
+}
+if (!process.env.SITE_URL_PITCHOU) {
+  throw new TypeError(`Variable d'environnement SITE_URL_PITCHOU manquante`);
+}
+
+console.log("NODE_ENV", process.env.NODE_ENV);
+
+await miseEnPlaceSecretGeoMCE();
 
 export const handle: Handle = async ({ event, resolve }) => {
   const start = Date.now();
