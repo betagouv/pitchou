a16665035d1db35518424c070d94dbc641d0dde5
Nicolas Cura <nicolas.cura@icloud.com>
2026-05-24 15:49:42 +0200

    feat(front): remplace store.js par store réactif Svelte 5

---
+++ scripts/front-end/dataPaths.ts
@@ -0,0 +1,5 @@
+export const ESPECES_DATA = "/data/liste-espèces-protégées.csv";
+export const ACTIVITES_METHODES_MOYENS_DE_POURSUITE_DATA =
+  "/data/activites-methodes-moyens-de-poursuite.ods";
+export const SCHEMA_DS_88444 =
+  "/data/démarche-numérique/schema-DS/derogation-especes-protegees.json";
