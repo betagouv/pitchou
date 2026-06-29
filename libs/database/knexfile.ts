b228fdbbe5e8fe5c3a24534413e340ded51c1485
Clémence Fernandez <clemencefrz@gmail.com>
2026-06-29 17:06:55 +0200

    Add `staging` knex env (with seeds) in knexfile.ts

--- libs/database/knexfile.ts
+++ libs/database/knexfile.ts
@@ -29,6 +29,23 @@ const config: Record<string, Knex.Config> = {
       directory: "./migrations",
     },
   },
+
+  // Same as prod, but with seeds
+  staging: {
+    client: "postgresql",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 0,
+      max: 30,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds/dev",
+    },
+  },
 };
 
 export default config;
