fe9b3144421bd61243883f3daea244f328748380
Clémence Fernandez <clemencefrz@gmail.com>
2026-07-01 15:26:53 +0200

    chore: Create PorteurDeProjet type file (libs/types/src/PorteurDeProjet.ts)

---
+++ libs/types/src/PorteurDeProjet.ts
@@ -0,0 +1,23 @@
+export type PorteurDeProjetPersonneMorale = {
+  type: "personne morale";
+  siret: string | null;
+  raison_sociale: string | null;
+  adresse: string | null;
+  nom_representant: string | null;
+  prenom_representant: string | null;
+  qualite_representant: string | null;
+  telephone: string | null;
+  email: string | null;
+};
+
+export type PorteurDeProjetPersonnePhysique = {
+  type: "personne physique";
+  nom: string | null;
+  prenom: string | null;
+  qualification: string | null;
+  adresse: string | null;
+  telephone: string | null;
+  email: string | null;
+};
+
+export type PorteurDeProjet = PorteurDeProjetPersonneMorale | PorteurDeProjetPersonnePhysique;
