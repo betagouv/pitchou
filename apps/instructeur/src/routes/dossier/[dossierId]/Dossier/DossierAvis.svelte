e25071ee58ae97d7cc8d8443f557cb2f8a72cb11
Clémence Fernandez <clemencefrz@gmail.com>
2026-07-13 11:56:38 +0200

    feat(avis tab): add guidelines for taking cnpn

--- apps/instructeur/src/routes/dossier/[dossierId]/Dossier/DossierAvis.svelte
+++ apps/instructeur/src/routes/dossier/[dossierId]/Dossier/DossierAvis.svelte
@@ -61,6 +61,47 @@
     Ajouter un avis ou une saisine
   </button>
 </div>
+<aside class="protocole-cnpn">
+  <h3 class="protocole-cnpn__titre">Vous devez saisir le CNPN ?</h3>
+  <p class="protocole-cnpn__intro">Voici le protocole&nbsp;:</p>
+  <ol>
+    <li>Vérifier que le dossier est prêt (espèces, impacts, etc.)</li>
+    <li>
+      Générer les documents&nbsp;:
+      <br />
+      <a
+        class="fr-link"
+        href="https://betagouv.github.io/pitchou/instruction/document-types/modeles.html"
+        target="_blank"
+        rel="noopener external">Accéder aux modèles</a
+      >
+      <ul>
+        <li>le mail s'adressant au secrétariat du CNPN</li>
+        <li>la saisine du CNPN</li>
+      </ul>
+    </li>
+    <li>
+      Envoyer le mail au secrétariat du CNPN (avec toutes les PJ et précautions nécessaires)
+      <br />
+      <a
+        class="fr-link email-cnpn"
+        href="mailto:derogations-especes-protegees.et4.deb.dgaln@developpement-durable.gouv.fr"
+        >derogations-especes-protegees.et4.deb.dgaln@developpement-durable.gouv.fr</a
+      >
+    </li>
+    <li>Stocker la saisine CNPN dans cet onglet</li>
+    <li>Quand vous le recevrez, stocker l'avis CNPN dans cet onglet</li>
+  </ol>
+  <p class="protocole-cnpn__details">
+    <strong>Pour plus de détails&nbsp;: </strong>
+    <a
+      class="fr-link"
+      href="https://betagouv.github.io/pitchou/instruction/saisine-cnpn.html"
+      target="_blank"
+      rel="noopener external">Consulter la documentation dédiée</a
+    >
+  </p>
+</aside>
 
 <ModalAddPieceJointe id={idModalAddPieceJointeAvis} {dossier} source="ongletAvis" />
 
@@ -68,10 +109,46 @@
   .section-list-avis-expert {
     display: flex;
     flex-direction: column;
+    flex: 1 1 0;
+    min-width: 0;
   }
   .list-avis-expert {
     display: flex;
     flex-direction: column;
     gap: 1.5rem;
+  }
+  .protocole-cnpn {
+    flex: 0 0 30rem;
+    max-width: 100%;
+
+    &__titre {
+      margin-bottom: 1rem;
+    }
+
+    &__intro {
+      font-weight: 700;
+      margin-bottom: 1rem;
+    }
+
+    ol {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      margin: 0;
+    }
+
+    ul {
+      margin-top: 0.5rem;
+      margin-bottom: 0;
+    }
+
+    &__details {
+      margin-top: 1.5rem;
+      margin-bottom: 0;
+    }
+
+    .email-cnpn {
+      overflow-wrap: anywhere;
+    }
   }
 </style>
