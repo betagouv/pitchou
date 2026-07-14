cd8f7b069b5ea90c6c0991f84be58ed6fd9e8079
Clémence Fernandez <clemencefrz@gmail.com>
2026-07-14 13:09:05 +0200

    chore: remove useless comments

--- apps/admin/src/instrumentation.server.ts
+++ apps/admin/src/instrumentation.server.ts
@@ -13,6 +13,6 @@ Sentry.init({
   },
   // In the Beta Sentry instance, the rate limiting system is implemented at the Nginx level, and the rule applies per project.
   // To avoid being impacted by these limits, reduce the trace rate at the SDK level
-  tracesSampleRate: 0.1, // Traces: 10% is sufficient in production
-  sampleRate: 1.0, // Errors: Keep at 100%
+  tracesSampleRate: 0.1,
+  sampleRate: 1.0,
 });
